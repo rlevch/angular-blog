bfcae0706f4e68ce46e37d6b5e87a1166d2ca037
rlevch <rlevch@gmail.com>
2021-01-24 16:43:44 +0100

    Auth Service

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {User} from '../../shared/interfaces';
+import {AuthService} from '../shared/services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -11,7 +13,7 @@ export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor() {
+  constructor(private authService: AuthService, private router: Router) {
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)])
@@ -31,6 +33,11 @@ export class LoginPageComponent implements OnInit {
       email: this.form.value.email,
       password: this.form.value.password
     };
+
+    this.authService.login(user).subscribe(() => {
+      this.form.reset();
+      this.router.navigate(['admin', 'dashboard']).then();
+    });
 
   }
 }
