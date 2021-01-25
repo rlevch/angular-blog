f23e45214fc97d8abf0cd692fa76832ddb4673df
rlevch <rlevch@gmail.com>
2021-01-25 13:31:04 +0100

    Creating a post's form

--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -9,3 +9,11 @@ export interface FireBaseAuthResponse {
   idToken: string;
   expiresIn: string;
 }
+
+export interface Post {
+  id?: string;
+  title: string;
+  text: string;
+  author: string;
+  date: Date;
+}
